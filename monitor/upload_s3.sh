ca14ebaffcba504e48dd528eb8665feb48259579
Tan Ho <tan@tanho.ca>
2026-09-12 16:01:44 +0000

    set verbose upload

--- monitor/upload_s3.sh
+++ monitor/upload_s3.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 set -euxo pipefail
-rclone copyto /home/tan/dry-age-monitor/logs sunlake-r2:sunlake/dry-age-monitor/logs --s3-no-head
-
+rclone copyto /home/tan/dry-age-monitor/logs sunlake-r2:sunlake/dry-age-monitor/logs --s3-no-head -v
